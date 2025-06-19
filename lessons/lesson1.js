3ea9580b76db703be6d3730c678cdf7484736460
Aleksandra Karic <aleksandrav86@gmail.com>
2025-06-19 14:53:46 +0200

    fix const variable

--- lessons/lesson1.js
+++ lessons/lesson1.js
@@ -14,14 +14,13 @@ sex = "Male"
 console.log(age)
 
 age = "6"
-console.log(age) // ako ovo pokrenemo prikazaće se i 5 i 6 za age, zato što varijable (var) 
-                 // nam to omogućava, moguće je da se override varijabla
+console.log(age) // ako ovo pokrenemo prikazaće se i 5 i 6 za age, zato što varijable (var) nam to omogućava, moguće je da se override varijabla
 
 // constants
 
 const occupation = "Engineer" // mora se deklarisati i inicijalizovati odmah
 console.log(occupation)
-occupation = "driver" // nemoguće je promeniti konstantu
+// occupation = "driver" - nemoguće je promeniti konstantu
 
 // data types
 var middleName = "David" // string, može i u '' navodnicima u JS
---
+++ lessons/lesson2.js
@@ -0,0 +1,10 @@
+// Concatination and Interpolation
+
+var price = 50
+var itemName = "Cup"
+
+var messageToPrint = "The price for your "+itemName+" is "+price+ " dollars."  //concatination
+var messageToPrint2 = `The price for your ${itemName} is ${price} dollars.` // interpolation - specifično za JS 
+
+console.log(messageToPrint)
+console.log(messageToPrint2)
