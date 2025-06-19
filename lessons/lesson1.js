f31c0db1d593c4baa9d5e6a70406b1ce6c8bcbc1
Aleksandra Karic <aleksandrav86@gmail.com>
2025-06-19 14:11:04 +0200

    added variables type

--- lessons/lesson1.js
+++ lessons/lesson1.js
@@ -2,11 +2,30 @@
 
 console.log("Hello World!")
 
-// 2. Variables
+// variables
 
 var firstName = "John"   // samo dok traje izvršenje programa
 let lastName = "Smith"
 console.log(firstName)
 
 var age, dateOfBirth, sex
+age = "5"
+sex = "Male"
 console.log(age)
+
+age = "6"
+console.log(age) // ako ovo pokrenemo prikazaće se i 5 i 6 za age, zato što varijable (var) 
+                 // nam to omogućava, moguće je da se override varijabla
+
+// constants
+
+const occupation = "Engineer" // mora se deklarisati i inicijalizovati odmah
+console.log(occupation)
+occupation = "driver" // nemoguće je promeniti konstantu
+
+// data types
+var middleName = "David" // string, može i u '' navodnicima u JS
+var ageOfBirth = 25 // number
+var isHeMarried = false // boolean
+var yearsInMarriage = null // neočekuje se nikakava vrednost ako je nema
+var numberOfCars = undefined // undefined
