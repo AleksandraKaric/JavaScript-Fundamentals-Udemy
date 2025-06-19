554184ece89dfa8162e8ffa2b29cd2fae0f0b7d2
Aleksandra Karic <aleksandrav86@gmail.com>
2025-06-19 15:40:38 +0200

    added relational and equality operators

---
+++ lessons/lesson4.js
@@ -0,0 +1,17 @@
+// Relational and comparison operators
+
+// > more than
+// < less than
+// >= more than equal
+// <= less than equal
+
+
+var result = 6 >= 5  
+console.log(result) // reultati su true ili false
+
+// Equality operators
+
+var x = 1
+
+console.log(x == '1') // lose comparison - proverava samo vrednost, ne i tip varijable
+console.log(x === 1) // strict comparison - proverava i vrednost i tip varijable
